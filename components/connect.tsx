e54019fb383a20b95e338ec4ee0c291244fa9001
uzochukwuv <vic.ezealor@gmail.com>
2025-05-21 12:04:18 +0100

    added disconnect

--- components/connect.tsx
+++ components/connect.tsx
@@ -2,27 +2,31 @@
 import { config } from '@/config/wagmi'
 import { Button } from '@heroui/button'
 import { sepolia } from 'viem/chains'
-import { Connector, useAccount, useConnect } from 'wagmi'
+import { Connector, useAccount, useConnect, useDisconnect } from 'wagmi'
 
 
 export function WalletOptions() {
-  const { connectors, connect } = useConnect({config:config})
-  const {address , isConnected} = useAccount()
+  const { connectors, connectAsync, data} = useConnect({ config: config })
+  const { address, isConnected } = useAccount()
+  const {disconnect} = useDisconnect()
 
-  return isConnected ? <Button
-  key={connectors[0].uid}
-onPress={()=>connect({connector:connectors[0]})}
-className="text-sm font-normal text-default-600 bg-success-200 "
+  return !isConnected ? connectors.map((c)=>{
+    return <Button
+    key={c.uid}
+    onPress={() => connectAsync({ connector: c })}
+    className="text-sm font-normal text-default-600 bg-success-200 "
 
-variant="solid"
->
-Connected
-</Button> : <Button
-        key={connectors[0].uid}
-    onPress={()=>connect({connector:connectors[0]})}
-  className="text-sm font-normal text-default-600 bg-default-100"
-  variant="solid"
->
-  Connect Wallet
-</Button>
+    variant="solid"
+  >
+    Connect Wallet
+  </Button> 
+  }) : <Button
+  
+    onPress={() => disconnect()}
+    className="text-sm font-normal text-default-600 bg-success-200 "
+
+    variant="solid"
+  >
+    Connected
+  </Button> 
 }
